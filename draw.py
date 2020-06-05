51f693354b62e6bfb693788ec5695abe60225ca7
yukiue <45696312+yukiue@users.noreply.github.com>
2020-06-06 01:50:49 +0900

    calculate drawing positions

--- draw.py
+++ draw.py
@@ -12,5 +12,20 @@ with open('list.txt', 'r') as f:
         g.add_edge(sep[0], sep[1], weight=sep[2])
 
 # nx.draw(g)
-nx.draw_spectral(g)
-plt.show()
+# nx.draw_spectral(g)
+# plt.show()
+
+# laplacian = nx.laplacian_matrix(g)
+# print(laplacian)
+
+A = nx.to_numpy_array(g)
+D = np.diag(np.sum(A, axis=1))
+L = D - A
+
+eigenval, eigenvec = np.linalg.eig(L)
+
+for i in range(len(eigenvec)):
+    eigenvec[i] = eigenvec[i] / np.linalg.norm(eigenvec[i])
+
+for i in range(nx.number_of_nodes(g)):
+    print(i, eigenvec[1][i], eigenvec[2][i], eigenvec[3][i])
