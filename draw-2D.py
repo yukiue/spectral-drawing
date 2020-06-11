bfdd9a699e44b6df70505d69cb92fdd4ab152aa1
yukiue <45696312+yukiue@users.noreply.github.com>
2020-06-11 22:20:10 +0900

    add program to draw path-graph

---
+++ draw-2D.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+
+g = nx.Graph()
+
+with open('path-graph.txt', 'r') as f:
+    for line in f:
+        sep = line.split()
+        g.add_edge(sep[0], sep[1])
+
+# nx.draw(g)
+# nx.draw_spectral(g)
+# plt.show()
+
+n = len(g.nodes)
+A = np.zeros((n, n))
+
+for i in sorted(g.nodes(), key=lambda i: int(i)):
+    for j in sorted(g.nodes(), key=lambda i: int(i)):
+        if (i, j) in g.edges():
+            A[int(i) - 1, int(j) - 1] = 1
+
+D = np.diag(np.sum(A, axis=1))
+L = D - A
+
+eigenval, eigenvec = np.linalg.eigh(L)
+
+eigenvec = eigenvec.T
+
+for i in range(len(eigenvec)):
+    eigenvec[i] = eigenvec[i] / np.linalg.norm(eigenvec[i])
+
+x = list(eigenvec[1])
+y = list(eigenvec[2])
+
+for i in range(len(eigenvec)):
+    print(i + 1, x[i], y[i])
+
+fig, ax = plt.subplots()
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.plot(x, y, marker='o', linestyle='None', color='black')
+for i, j in g.edges():
+    ax.plot([x[int(i) - 1], x[int(j) - 1]], [y[int(i) - 1], y[int(j) - 1]],
+            color='green')
+# plt.show()
+fig.savefig('path-graph.jpg')
